1b856297023498639a9e003a1b350fb5611bcf59
ckorzen <ckorzen@users.noreply.github.com>
2020-11-27 14:40:08 +0100

    Fixed some checkstyle issues.

--- src/main/java/textextraction/common/models/Color.java
+++ src/main/java/textextraction/common/models/Color.java
@@ -1,6 +1,7 @@
 package textextraction.common.models;
 
 import java.util.Arrays;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
--- src/main/java/textextraction/common/models/Page.java
+++ src/main/java/textextraction/common/models/Page.java
@@ -2,6 +2,7 @@ package textextraction.common.models;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
